d44fff970803da86f49ae35ae9057302c3d1143f
mahmoud <mahmoud.kheyrati.fard@gmail.com>
2023-03-08 16:02:13 +0330

    implement REST APIs in the server

--- hw2/server/main.go
+++ hw2/server/main.go
@@ -7,13 +7,23 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	_ "github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/utils"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 
 	"io/ioutil"
 	"math/rand"
 )
+
+const USER_DATABASE = "user"
+const PERSON_COLLECTION = "person"
+
+type Person struct {
+	Name    string `json:"name"`
+	Address string `json:"address"`
+}
 
 func main() {
 	ctx := context.Background()
@@ -29,18 +39,76 @@ func main() {
 
 	// create new person
 	app.Put("/person", func(ctx *fiber.Ctx) error {
+		person := Person{}
+		err := ctx.BodyParser(&person)
+
+		if err != nil {
+			ctx.Status(fiber.StatusBadRequest)
+			ctx.JSON(fiber.Map{"error": "bad request"})
+			return nil
+		}
+		fmt.Println("person", person)
+		res, err := mongoClient.Database(USER_DATABASE).Collection(PERSON_COLLECTION).InsertOne(context.Background(), person)
+		if err != nil {
+			ctx.Status(fiber.StatusInternalServerError)
+			ctx.JSON(fiber.Map{"error": "can not create person"})
+		}
+		ctx.JSON(fiber.Map{"id": res.InsertedID})
 		return nil
 	})
 	// return list of all persons
-	app.Get("/person", func(ctx *fiber.Ctx) error {
+	app.Get("/person/all", func(ctx *fiber.Ctx) error {
+		var persons []bson.M
+		res, err := mongoClient.Database(USER_DATABASE).Collection(PERSON_COLLECTION).Find(context.Background(), bson.M{})
+		if err != nil {
+			ctx.Status(fiber.StatusInternalServerError)
+			ctx.JSON(fiber.Map{"error": "can not get persons"})
+			return nil
+		}
+		err = res.All(context.Background(), &persons)
+		if err != nil {
+			ctx.Status(fiber.StatusInternalServerError)
+			ctx.JSON(fiber.Map{"error": "can not get persons"})
+			return nil
+		}
+		fmt.Println("persons: ", persons)
+		ctx.JSON(persons)
 		return nil
 	})
 	// get person by id
 	app.Get("/person/:id", func(ctx *fiber.Ctx) error {
+		id, err := primitive.ObjectIDFromHex(utils.CopyString(ctx.Params("id")))
+		if err != nil {
+			ctx.Status(fiber.StatusInternalServerError)
+			ctx.JSON(fiber.Map{"error": "can not get persons"})
+			return nil
+		}
+		var person Person
+		err = mongoClient.Database(USER_DATABASE).Collection(PERSON_COLLECTION).FindOne(context.Background(), bson.M{"_id": id}).Decode(&person)
+		if err != nil {
+			ctx.Status(fiber.StatusInternalServerError)
+			ctx.JSON(fiber.Map{"error": "can not get person"})
+			return nil
+		}
+		ctx.JSON(person)
 		return nil
 	})
 	// delete person by id
 	app.Delete("/person/:id", func(ctx *fiber.Ctx) error {
+		id, err := primitive.ObjectIDFromHex(utils.CopyString(ctx.Params("id")))
+		if err != nil {
+			ctx.Status(fiber.StatusInternalServerError)
+			ctx.JSON(fiber.Map{"error": "can not delete person"})
+			return nil
+		}
+		var person Person
+		err = mongoClient.Database(USER_DATABASE).Collection(PERSON_COLLECTION).FindOneAndDelete(context.Background(), bson.M{"_id": id}).Decode(&person)
+		if err != nil {
+			ctx.Status(fiber.StatusInternalServerError)
+			ctx.JSON(fiber.Map{"error": "can not get persons"})
+			return nil
+		}
+		ctx.JSON(person)
 		return nil
 	})
 
@@ -52,13 +120,13 @@ func main() {
 		if err != nil {
 			ctx.Status(fiber.StatusInternalServerError)
 			err = ctx.JSON(fiber.Map{"error": "can not serve file"})
-			return err
+			return nil
 		}
 		file, err := getChecksumOfFile(path)
 		if err != nil {
 			ctx.Status(fiber.StatusInternalServerError)
 			ctx.JSON(fiber.Map{"error": "can not serve file"})
-			return err
+			return nil
 		}
 		ctx.Set("checksum", file)
 		ctx.Set(fiber.HeaderContentDisposition, fmt.Sprintf("attachment; filename=\"%s\"", fileName))
@@ -66,7 +134,7 @@ func main() {
 		if err != nil {
 			ctx.Status(fiber.StatusInternalServerError)
 			ctx.JSON(fiber.Map{"error": "can not serve file"})
-			return err
+			return nil
 		}
 		return nil
 	})
