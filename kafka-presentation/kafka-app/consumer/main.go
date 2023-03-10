72738a5763bc48422945ad112e97a06d483072ca
mahmoud <mahmoud.kheyrati.fard@gmail.com>
2023-03-10 17:09:15 +0330

    add simple consumer

--- kafka-presentation/kafka-app/consumer/main.go
+++ kafka-presentation/kafka-app/consumer/main.go
@@ -1,1 +1,43 @@
 package main
+
+import (
+	"fmt"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func main() {
+	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers":  "localhost:9092",
+		"group.id":           "foo",
+		"group.instance.id":  "1",
+		"enable.auto.commit": false,
+		"auto.offset.reset":  "earliest"}) // earliest, latest
+	if err != nil {
+		panic(err)
+	}
+	err = consumer.SubscribeTopics([]string{"stream"}, func(consumer *kafka.Consumer, event kafka.Event) error {
+		fmt.Println("event happens: ", event.String())
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	justRead(consumer)
+
+}
+
+func justRead(consumer *kafka.Consumer) {
+
+	for {
+		message, err := consumer.ReadMessage(-1)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		fmt.Println("consumer message content: ", string(message.Value))
+
+	}
+
+}
