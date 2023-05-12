8641c76b8e0a7b16544ed343a46f14b3f9ef0e32
mahmoud <mahmoud.kheyrati.fard@gmail.com>
2023-05-12 20:23:02 +0330

    add argument for the kafkaConsumer

--- kafka-presentation/kafka-app/consumer/main.go
+++ kafka-presentation/kafka-app/consumer/main.go
@@ -1,15 +1,31 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"log"
+)
+
+var (
+	groupId         = ""
+	groupInstanceId = ""
+	commitMode      = ""
+	batchSize       = 1
 )
 
 func main() {
+	flag.StringVar(&groupId, "groupId", "foo", "groupId of the kafka consumer")
+	flag.StringVar(&groupInstanceId, "groupInstanceId", "1", "groupInstanceId of the kafka consumer")
+	flag.StringVar(&commitMode, "commitMode", "justRead", "commit mode of the kafka consumer. should be one of these values: justRead, syncCommit, asyncCommit")
+	flag.IntVar(&batchSize, "batchSize", 1, "batchSize to commit")
+	flag.Parse()
+	log.Println("start kafkaConsumer with {groupId=", groupId, " and groupInstanceId=", groupInstanceId, "commitMode=", commitMode, " batchSize=", batchSize, "}")
+
 	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
 		"bootstrap.servers":  "localhost:9092",
-		"group.id":           "foo",
-		"group.instance.id":  "1",
+		"group.id":           groupId,
+		"group.instance.id":  groupInstanceId,
 		"enable.auto.commit": false,
 		"auto.offset.reset":  "earliest"}) // earliest, latest
 	if err != nil {
@@ -23,9 +39,16 @@ func main() {
 		panic(err)
 	}
 
-	//justRead(consumer)
-	//syncCommit(consumer, 1000)
-	AsyncCommit(consumer, 10)
+	switch commitMode {
+	case "justRead":
+		justRead(consumer)
+	case "syncCommit":
+		syncCommit(consumer, batchSize)
+	case "asyncCommit":
+		AsyncCommit(consumer, batchSize)
+	default:
+		log.Println("commitMode should be on of these values: justRead, syncCommit, asyncCommit")
+	}
 
 }
 
@@ -54,7 +77,7 @@ func syncCommit(consumer *kafka.Consumer, commitBatchSize int) {
 		}
 
 		//// actual process on data
-		//fmt.Println("consumer message content: ", string(message.Value))
+		fmt.Println("consumer message content: ", string(message.Value))
 
 		count++
 		if count == commitBatchSize {
@@ -80,7 +103,7 @@ func AsyncCommit(consumer *kafka.Consumer, commitBatchSize int) {
 		}
 
 		//// actual process on data
-		//fmt.Println("consumer message content: ", string(message.Value))
+		fmt.Println("consumer message content: ", string(message.Value))
 
 		count++
 		if count == commitBatchSize {
